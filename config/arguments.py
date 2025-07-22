a54ad0ceaf22f73c7f12379c679e1238ee26c71a
Brench <brenchchen.77@gmail.com>
2025-07-23 01:51:49 +0800

    update arguments.py

--- config/arguments.py
+++ config/arguments.py
@@ -25,3 +25,37 @@ class DataArguments:
         default = "data/eval.json",
         metadata = {"HELP": "Eval Data Path"}
     )
+
+@dataclass
+class TrainingArguments(HFTrainingArguments):
+    """
+    训练参数相关配置，FROM HuggingFace TrainingArguments
+    """
+    cache_dir: Optional[str] = field(default=None, metadata={"HELP": "Cache Dir"})
+    optimizer: str = field(default = "adamw_torch")
+    model_max_length: int = field(
+        default = 512,
+        metadata={"HELP": "Maximum Length of Sequence To be Padded"}
+    )
+    lora: bool = field(default = False, metadata={"HELP": "LoRa Enable"})
+    qlora: bool = field(default = False, metadata={"HELP": "QLoRa Enable"})
+
+    # DPO相关参数
+    dpo: bool = field(default = False, metadata={"HELP": "Use DPO"})
+    dpo_beta: float = field(
+        default = 0.1,
+        metadata={"HELP": "DPO Loss Function Beta Argument, controls the degree to which the strategy model loses the reference model"}
+    )
+    reference_model_path: Optional[str] = field(
+        default = None,
+        metadata={"HELP": "Reference Model Path, if None then using main model"}
+    )
+
+    # SFT损失权重参数
+    sft_loss_weight: float = field(
+        default = 0.1,
+        metadata={"HELP": "SFT Loss Weight, combine with DPO Training"}
+    )
+if __name__ == "__main__":
+    print(DataArguments.train_data_path)
+    print(DataArguments.eval_data_path)
